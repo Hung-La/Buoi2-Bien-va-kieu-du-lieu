c7a98ba64f2a62cef420f095a825ef7b49e257b0
Hung-La <hunglabk@gmail.com>
2025-12-26 23:52:54 +0700

    Buoi 2 - Bien - Kieu du lieu

---
+++ Buoi2-Bien KieuDuLieu/Buoi2-Bien KieuDuLieu/Program.cs
@@ -0,0 +1,66 @@
+﻿
+namespace Buoi2_Bien_KieuDuLieu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? age = null;
+            int ageV2 = 20;
+            Console.WriteLine("Hello, World! 2");
+
+            float test = 2f;
+            Console.WriteLine(test);
+
+            // Quy tắc đặt tên biến:
+            // 1. Biến cục bộ -> camelCase
+            // 2. Biến có ý nghĩa -> Đặt tên rõ ràng
+
+            string fullName = "Hưng Lã";
+
+            // Đặt tên biến theo từng loại
+            // 1. Biến số
+            int count;
+            int totalProduct;
+            int maxValue;
+            int minValue;
+            double score;
+
+            // 2. Biến chuỗi (string)
+            string emailAddress;
+
+            // 3. Biến mảng/ danh sách
+            int[] scores;
+            List<string> scoreList;
+
+            // 4. Biến hằng số (constant) -> Khai báo theo kiểu PascalCase;
+            const int TotalScore = 15;
+
+            // Khởi tạo biến - gán lại giá trị cho biến
+
+            // Khai báo lúc biên dịch
+            int x = 0;
+            x = 10;
+            Console.WriteLine("Giá trị x = " + x);
+
+            // Khai báo lúc chạy
+            Console.Write("Nhap dia chi nha : ");
+            string? address = Console.ReadLine();
+            Console.WriteLine("Dia chi Nha : " + address);
+
+            // 1 vài câu lệnh nhanh trong C#
+            // Ctrl + D : Duplicate
+            // Ctrl + X : Cut
+            // Giữ Alt + Kéo chuột : sửa nhiều dòng
+            // Giữ Alt + lên/xuống 
+            // Ctrl + R, Ctrl + R : Rename biến
+
+            Console.WriteLine("Hiển thị");
+            Console.WriteLine("Hiển thị");
+            Console.WriteLine("Hiển thị");
+            Console.WriteLine("Hiển thị");
+            Console.WriteLine("Hiển thị");
+            Console.WriteLine("Dia chi Nha : " + address);
+        }
+    }
+}
---
+++ Buoi2-Bien KieuDuLieu/Buoi2-KieuDuLieu/Program.cs
@@ -0,0 +1,145 @@
+﻿namespace Buoi2_KieuDuLieu
+{
+    internal class Program
+    {
+        static void Print(object obj)
+        {
+            Console.WriteLine("Giá trị object = " + obj);
+            Console.WriteLine(obj.GetType());
+        }
+
+        static void TestGeneric<T>(T obj)
+        {
+            Console.WriteLine("Giá trị object = " + obj);
+            Console.WriteLine(obj.GetType());
+        }
+        static void Main(string[] args)
+        {
+            // Kiểu dữ liệu của biến
+            // 1. Kiểu dữ liệu giá trị
+            // 2. Kiểu dữ liệu tham chiếu
+            // 3. Kiểu dự liệu con trỏ
+
+
+            // 1. Kiểu dự liệu hay dùng
+            // int, long, double ,float
+            // số nguyên
+            int totalInt = -2000000000;
+            long totalLong = -30000000000000000;
+
+            // số thực
+            float totalFloat = 444.56f;
+            double totalDouble = 600.30;
+            decimal totalDecimal = 124657987.1324564987m;
+
+            // kiểu ký tự
+            char test = '@';
+
+            // kiểu logic
+            bool isCheck = false;
+            bool isChecked = true;
+
+            // Khai báo kết quả
+            char a = 'G';
+            int i = 89;
+            short s = 56;
+            long l = 4564;
+            uint ui = 95;
+            ushort us = 76;
+            ulong ul = 3624573;
+            double d = 8.358674532;
+            float f = 3.7330645f;
+            decimal dec = 389.5m;
+
+            // In kết quả
+            Console.WriteLine("char: " + a);
+            Console.WriteLine("integer: " + i);
+            Console.WriteLine("short: " + s);
+            Console.WriteLine("long: " + l);
+            Console.WriteLine("float: " + f);
+            Console.WriteLine("double: " + d);
+            Console.WriteLine("decimal: " + dec);
+            Console.WriteLine("Unsigned integer: " + ui);
+            Console.WriteLine("Unsigned short: " + us);
+            Console.WriteLine("Unsigned long: " + ul);
+
+            //Console.Write("Ky tu nhap : ");
+            //string? chuoi = Console.ReadLine();
+            //Console.WriteLine("Ky tu da nhap : " + chuoi);
+
+            //Console.Write("Nhap so Nguyen : ");
+            //int soNguyen = Convert.ToInt16(Console.ReadLine());
+            //Console.WriteLine("So Nguyen da nhap : " + soNguyen);
+
+            //Console.Write("Nhap so Thuc : ");
+            //double soThuc = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("So Nguyen da nhap : " + soThuc);
+
+            //Console.Write("Nhap so Decimal");
+            //decimal soDecimal = Convert.ToDecimal(Console.ReadLine());
+            //Console.WriteLine("So Decimal da nhap : " + soDecimal);
+
+            object className;
+            className = "PC11";
+            className = 20;
+
+            Console.WriteLine("Gia tri Obj: " + className);
+
+            //boxing
+            int intA = 10; // value type : kiểu giá trị
+            object obj = intA; // boxing
+
+            object objV2 = 10.0;
+            double intB = (double)objV2; // unboxing
+
+            Console.WriteLine("intB : " + intB);
+
+            // tại sao phải boxing và unboxing
+            // Vì C# vừa có kiểu dự liệu là kiểu gia trị (value type) và kiểu tham chiếu (reference type)
+            // mà nhiều lúc code chỉ làm việc với object
+            // thì boxing và unboxing nó là cái cầu nối
+            int testA = 10;
+            double testB = 20.5;
+            Print(testA);
+            Print(testB);
+
+            Console.WriteLine("------------------");
+            TestGeneric(testA);
+            TestGeneric(testB);
+
+            Console.WriteLine("------------------");
+            // 4. Kiểu con trỏ
+            unsafe
+            {
+                // Khai báo biến
+                int n = 10;
+                // Lưu địa chỉ của biến n vào biến con trỏ p
+                int* p = &n;
+                *p = 20;
+                n = 30;
+
+                Console.WriteLine("Value: {0}", n);
+                Console.WriteLine("Value *p: " + *p);
+                Console.WriteLine("Address: {0}", (int)p);
+            }
+
+
+            // Kiểu dữ liệu:
+            // 1. Kiểu giá trị (value type) : int, double, float, long
+            // Lưu trực tiếp giá trị
+            // gán biến -> copy giá trị
+            // hiệu suất cao và tiết kiệm
+            
+
+            // 2. Kiểu tham chiều (reference type) : string, array, class, object, interface, delegate
+            // là kiểu dữ liệu mà biến khai báo không chứa dữ liệu, chỉ chứa địa chỉ ô nhớ, rồi nó trỏ đến giá trị
+            // nhiều biến có thể trỏ chung 1 object
+            // mở rộng và linh hoạt
+
+
+            // Vùng nhớ Stack là vùng nhớ tạm thời lưu trữ giá trị của biến cục bộ hoặc địa chỉ biến(tham chiếu).
+            // Vung nhớ Heap là vùng nhớ lâu dài vẫn ở trên RAM, lưu trữ giá trị boxing, có hiệu suất thấp hơn Stack. 
+
+        }
+    }
+}
